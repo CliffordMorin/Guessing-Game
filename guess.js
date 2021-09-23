d0224cde0977f2cb53454b2df62145a14d38db51
Clifford Morin <cemorin21@gmail.com>
2021-09-23 03:33:59 -0400

    to guewss

--- guess.js
+++ guess.js
@@ -1,10 +1,24 @@
-//Guess a random number
-//Give user the ability to guess
+//Guess a random number DONE
+//Give user the ability to guess Done
 //If they guess and they are wrong, ask them again (hint);
 //If they win - say that they won
 
 function guessGame() {
 	let randomNr = Math.floor(Math.random() * 11);
+	let guess;
+
+	do {
+		guess = prompt('Guess a number between 1 and 10');
+		console.log(guess, randomNr);
+		if (randomNr > guess) {
+			console.log('you guessed to low');
+		} else if (randomNr < guess) {
+			console.log('you guessed too high');
+		}
+	} while (guess != randomNr);
+	{
+		console.log('you won');
+	}
 }
 
 guessGame();
