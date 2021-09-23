871d8bc5acff26eecb9403a67df901b8519c5862
Clifford Morin <cemorin21@gmail.com>
2021-09-23 03:22:35 -0400

    initial commit

---
+++ guess.js
@@ -0,0 +1,11 @@
+//Guess a random number
+//Give user the ability to guess
+//If they guess and they are wrong, ask them again (hint);
+//If they win - say that they won
+
+function guessGame() {
+	let randomNr = Math.random() * 10;
+	console.log(randomNr);
+}
+
+guessGame();
